a0fc2324183ed655b229f5dcf11df94d51c68dc8
yasin <buntyvirani82@gmail.com>
2021-05-28 22:26:11 -0700

    filtered out listof bowlers for ncca

---
+++ nccaFilterName.js
@@ -0,0 +1,23 @@
+const playerData = require("./NCCAPlayerName.json");
+const fs = require("fs");
+
+var newArr = [];
+var newData = playerData.map((record) => {
+  record.allTeams.map((teams) => {
+    if (teams.listOfPlayer.length !== 0) {
+      const newRecord = teams.listOfPlayer.filter(
+        (x) =>
+          !x.playerRole.includes("Bowler") && !x.playerRole.includes("Bowler")
+      );
+      newArr.push(newRecord);
+    }
+  });
+  return newData;
+});
+console.log(newArr);
+
+fs.writeFile("./nccaFilteredName.json", JSON.stringify(newArr), (err) => {
+  if (err) {
+    console.log("write error: " + err);
+  }
+});
